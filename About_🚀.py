97d9ba1dad62f2fcd597189c02141bdc248899ae
Uğur Can Kıvanç <87431893+Exedeus21@users.noreply.github.com>
2022-06-11 23:55:22 +0300

    Update About_🚀.py

--- About_🚀.py
+++ About_🚀.py
@@ -14,27 +14,27 @@ st.title("ABOUT")
 st.write("""##### This repo is a part of K136. Kodluyoruz & Istanbul Metropolitan Municipality Data Science Bootcamp. The project aims to produce a machine learning model for home price estimation. The model was built on the Kaggle House Prices - Advanced Regression Techniques competition dataset. """)
 st.write(""" The dataset contains the following features: \n
 
-- MSSubClass: Identifies the type of dwelling involved in the sale
-- MoSold: Month Sold (MM)
-- OverallQual: Rates the overall material and finish of the house
-- OveralCond: Rates the overall condition of the house
-- YrSold: Year Sold (YYYY)
-- LotArea: Lot size in square feet
-- LotFrontage: Linear feet of street connected to property
-- BsmtUnfSF: Unfinished square feet of basement area
-- GrLivArea: Above grade (ground) living area square feet
-- GarageArea: Size of garage in square feet
-- TotalBsmtSF: Total square feet of basement area
-- YearBuilt: Year house was built
-- 1stFlrSF: First Floor square feet
-- BsmtFinSF1: Type 1 finished square feet
-- OpenPorchSF: Open porch square feet
-- MasVnrArea: Masonry veneer square feet
-- YearRemodAdd: Remodel date (same as construction date if no remodeling or additions)
-- WoodDeckSF: Wood deck area in square feet
-- GarageYrBlt: Year garage was built
-- 2ndFlrSF: Second floor square feet
-- SalePrice: Sale price""")
+* **MSSubClass**: Identifies the type of dwelling involved in the sale
+* **MoSold**: Month Sold (MM)
+* **OverallQual**: Rates the overall material and finish of the house
+* **OveralCond**: Rates the overall condition of the house
+* **YrSold**: Year Sold (YYYY)
+* **LotArea**: Lot size in square feet
+* **LotFrontage**: Linear feet of street connected to property
+* **BsmtUnfSF**: Unfinished square feet of basement area
+* **GrLivArea**: Above grade (ground) living area square feet
+* **GarageArea**: Size of garage in square feet
+* **TotalBsmtSF**: Total square feet of basement area
+* **YearBuilt**: Year house was built
+* **1stFlrSF**: First Floor square feet
+* **BsmtFinSF1**: Type 1 finished square feet
+* **OpenPorchSF**: Open porch square feet
+* **MasVnrArea**: Masonry veneer square feet
+* **YearRemodAdd**: Remodel date (same as construction date if no remodeling or additions)
+* **WoodDeckSF**: Wood deck area in square feet
+* **GarageYrBlt**: Year garage was built
+* **2ndFlrSF**: Second floor square feet
+* **SalePrice**: Sale price""")
 
 st.write("""## Model Development
 The model is based on a XGBoost algorithm.
