70eac90e772d3108ca7f581ecde6d915ad59348a
Fuat Akdemir <akdemir_fuat@hotmail.com>
2022-06-11 03:36:45 +0300

    About page updated.

--- About_🚀.py
+++ About_🚀.py
@@ -9,30 +9,31 @@ import xgboost
 
 # Display details of page 1
 
+st.set_page_config(page_title="About Page - House Price Prediction", page_icon="🚀")
 st.title("ABOUT")
 st.write("""##### This repo is a part of K136. Kodluyoruz & Istanbul Metropolitan Municipality Data Science Bootcamp. The project aims to produce a machine learning model for home price estimation. The model was built on the Kaggle House Prices - Advanced Regression Techniques competition dataset. """)
 st.write(""" The dataset contains the following features: \n
 
-- MSSubClass : Identifies the type of dwelling involved in the sale\n
-- MoSold: Month Sold (MM)\n
-- OverallQual: Rates the overall material and finish of the house\n
-- OveralCond: Rates the overall condition of the house\n
-- YrSold: Year Sold (YYYY)\n
-- LotArea: Lot size in square feet\n
-- LotFrontage: Linear feet of street connected to property\n
-- BsmtUnfSF: Unfinished square feet of basement area\n
-- GrLivArea: Above grade (ground) living area square feet\n
-- GarageArea: Size of garage in square feet\n
-- TotalBsmtSF: Total square feet of basement area\n
-- YearBuilt: Year house was built\n
-- 1stFlrSF: First Floor square feet\n
-- BsmtFinSF1: Type 1 finished square feet\n
-- OpenPorchSF: Open porch square feet\n
-- MasVnrArea: Masonry veneer square feet\n
-- YearRemodAdd: Remodel date (same as construction date if no remodeling or additions)\n
-- WoodDeckSF: Wood deck area in square feet\n
-- GarageYrBlt: Year garage was built\n
-- 2ndFlrSF: Second floor square feet\n
+- MSSubClass: Identifies the type of dwelling involved in the sale
+- MoSold: Month Sold (MM)
+- OverallQual: Rates the overall material and finish of the house
+- OveralCond: Rates the overall condition of the house
+- YrSold: Year Sold (YYYY)
+- LotArea: Lot size in square feet
+- LotFrontage: Linear feet of street connected to property
+- BsmtUnfSF: Unfinished square feet of basement area
+- GrLivArea: Above grade (ground) living area square feet
+- GarageArea: Size of garage in square feet
+- TotalBsmtSF: Total square feet of basement area
+- YearBuilt: Year house was built
+- 1stFlrSF: First Floor square feet
+- BsmtFinSF1: Type 1 finished square feet
+- OpenPorchSF: Open porch square feet
+- MasVnrArea: Masonry veneer square feet
+- YearRemodAdd: Remodel date (same as construction date if no remodeling or additions)
+- WoodDeckSF: Wood deck area in square feet
+- GarageYrBlt: Year garage was built
+- 2ndFlrSF: Second floor square feet
 - SalePrice: Sale price""")
 
 st.write("""## Model Development
